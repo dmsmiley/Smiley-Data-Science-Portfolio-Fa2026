456de17beb1624040bed55997657b53945fff15b
David M. Smiley <davidmsmiley@gmail.com>
2026-09-15 09:43:56 -0400

    Update my_app.py

--- week3/my_app.py
+++ week3/my_app.py
@@ -7,3 +7,9 @@ if st.button("Click me!"):
     st.write("🎉 You clicked the button! Nice work! 🚀")
 else:
     st.write("Click the button to see what happens...")
+
+import pandas as pd
+
+st.subheader("Exploring Our Dataset")
+
+df = pd.read_csv("C:\Users\dsmiley\Documents\GitHub\Smiley-Data-Science-Portfolio-Fa2026\week3\data\sample_data.csv")
